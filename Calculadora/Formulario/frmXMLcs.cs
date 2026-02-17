6981aa2ceea7fd3cb1102bb3429e58ec1c550933
aless789hc <24130789@correo.itlalaguna.edu.mx>
2026-02-17 15:47:28 -0600

    Se termina XML

--- Calculadora/Formulario/frmXMLcs.cs
+++ Calculadora/Formulario/frmXMLcs.cs
@@ -18,19 +18,23 @@ namespace Calculadora.Formulario
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            XElement xml = new XElement("Directorio");
-            
-            for (int i = 0; i < dgvPersonas.Rows.Count; i++)
+            XElement xml = new XElement("Personas");
+
+            for (int i = 0; i < dgvPersonas.Rows.Count-1; i++)
             {
                 xml.Add(
-                    new XElement("Persona"),
-                new XAttribute("ID", dgvPersonas.Rows[i].Cells[0].Value), 
-                new XElement("Nombre", dgvPersonas.Rows[i].Cells[1].Value), 
-                    new XElement("Telefono", dgvPersonas.Rows[i].Cells[2].Value));
+                new XElement("Persona",
+                    new XAttribute("ID", dgvPersonas.Rows[i].Cells[0].Value),
+                        new XElement("Nombre", dgvPersonas.Rows[i].Cells[1].Value),
+                         new XElement("Telefono", dgvPersonas.Rows[i].Cells[2].Value)));
+
 
                 
             }
-        xml.Save("Archivo.xml");
+            try { xml.Save("Archivo.xml");
+                MessageBox.Show("Guardado", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
         }
     }
 }
